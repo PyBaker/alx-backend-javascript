e4cfe906f49f6d6f1185a91d0fdc0aa617b0f611
PyBaker <75681212+PyBaker@users.noreply.github.com>
2022-09-05 16:53:44 +0200

    done with tasks

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency';
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this.amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
